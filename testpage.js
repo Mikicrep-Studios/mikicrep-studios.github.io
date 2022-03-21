7d49cdf68e577b94aa9af2e8de09a30a4475aae3
Mikicrep <92646867+Mikicrepstudios@users.noreply.github.com>
2022-03-21 19:47:05 +0100

    2.4

---
+++ testpage.js
@@ -0,0 +1,7 @@
+shei = window.screen.height
+swid = window.screen.width
+
+function updt() {
+    document.getElementById("SHEI").innerHTML = shei
+    document.getElementById("SWID").innerHTML = swid
+}
