ba95285a094c25e6956c1eddef7d2c947d66098e
Mikicrep <mikicrepoffical@gmail.com>
2024-10-26 21:31:32 +0200

    4.6.4

--- jsscripts/gallerydata.js
+++ jsscripts/gallerydata.js
@@ -1,6 +1,7 @@
 // gallerydata.js
 
 const galleryImages = [
+    { original: "/assets/artgallery/original/11.png", webp: "/assets/artgallery/webp/11.webp" },
     { original: "/assets/artgallery/original/10.png", webp: "/assets/artgallery/webp/10.webp" },
     { original: "/assets/artgallery/original/9.png", webp: "/assets/artgallery/webp/9.webp" },
     { original: "/assets/artgallery/original/8.png", webp: "/assets/artgallery/webp/8.webp" },
