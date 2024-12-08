a5cd7116f60e4c6d09920ce6adcebfe236f3265a
Mikicrep <mikicrepoffical@gmail.com>
2024-12-09 00:30:22 +0100

    4.8.16

--- projects/docs/mf/search/functions_a.js
+++ projects/docs/mf/search/functions_a.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['timecount_0',['TimeCount',['../d9/d0f/namespacecore.html#a3d2ea9bae70a098cbe6045b16a95e47c',1,'core']]]
+  ['setdrawcolor_0',['SetDrawColor',['../de/d00/namespacedraw.html#a47ed218c3a1917f6958f6d43247de0b0',1,'draw']]],
+  ['setwindowicon_1',['SetWindowIcon',['../d9/d0f/namespacecore.html#a1fb511e8957cf8995e78e8609453916f',1,'core']]]
 ];
