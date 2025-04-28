8a6c7b07f4627902d7cb222e04e0d10e2af7b2f1
Mikicrep <mikicrepoffical@gmail.com>
2025-04-28 21:39:58 +0200

    4.8.37

--- jsscripts/forms/discordverify.js
+++ jsscripts/forms/discordverify.js
@@ -4,9 +4,26 @@ function sendcode() {
   
     const url = `${apiSite}linkdiscord/${username}/${discordID}`;
   
-    fetch(url, {
-      method: 'GET',
-    })
+    fetch(url)
+      .then(response => {
+        if (response.ok) {
+          console.log("Request sent successfully!");
+        } else {
+          console.error(`Request failed with status: ${response.status}`);
+        }
+      })
+      .catch(error => {
+        console.error("An error occurred:", error);
+      });
+  }
+  
+  function sendcodelogin() {
+    const username = document.getElementById('user').value.toLowerCase();
+    const discordID = document.getElementById('sendcode').value;
+  
+    const url = `${apiSite}linkdiscord/${username}/${discordID}`;
+  
+    fetch(url)
       .then(response => {
         if (response.ok) {
           console.log("Request sent successfully!");
