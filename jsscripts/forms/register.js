c9f214add7492f872971c469a140ab563165123e
Mikicrep <mikicrepoffical@gmail.com>
2025-04-28 21:14:10 +0200

    4.8.36

--- jsscripts/forms/register.js
+++ jsscripts/forms/register.js
@@ -25,25 +25,3 @@ function sumbit() {
       document.getElementById("result").textContent = "Passwords do not match!";
   }
 }
-
-
-function sendcode() {
-  const username = document.getElementById('sendusername').value.toLowerCase();
-  const discordID = document.getElementById('sendcode').value;
-
-  const url = `${apiSite}linkdiscord/${username}/${discordID}`;
-
-  fetch(url, {
-    method: 'GET',
-  })
-    .then(response => {
-      if (response.ok) {
-        console.log("Request sent successfully!");
-      } else {
-        console.error(`Request failed with status: ${response.status}`);
-      }
-    })
-    .catch(error => {
-      console.error("An error occurred:", error);
-    });
-}
