5c7102765b0e239bc9737a7360d4679ef1f2a5fb
Mikicrep <mikicrepoffical@gmail.com>
2026-03-13 08:00:05 +0100

    4.9.15

--- jsscripts/blendergallerydata.js
+++ jsscripts/blendergallerydata.js
@@ -2,6 +2,10 @@
 
 const galleryImages = [
   {
+    original: '/assets/blendergallery/original/2.gif',
+    webp: '/assets/blendergallery/webp/2.gif'
+  },
+  {
     original: '/assets/blendergallery/original/1.gif',
     webp: '/assets/blendergallery/webp/1.gif'
   },
